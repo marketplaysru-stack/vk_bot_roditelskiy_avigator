5c1d15f4b28b5ab87741796dc3e610dd13f6885e
marketplaysru-stack <marketplaysru@gmail.com>
2026-07-27 12:40:53 +0300

    Add files via upload

--- generators/text/dummy.py
+++ generators/text/dummy.py
@@ -1,1 +1,5 @@
+from core.base import TextGenerator
 
+class DummyTextGenerator(TextGenerator):
+    def generate(self, topic: str) -> str:
+        return f"📢 {topic}\n\nЭто автоматический пост. Тема: {topic}."
