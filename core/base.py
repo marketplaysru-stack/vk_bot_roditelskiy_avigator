5b397e076a2aa34e241fb48e9091365f4ce6f79d
marketplaysru-stack <marketplaysru@gmail.com>
2026-07-27 12:40:03 +0300

    Add files via upload

--- core/base.py
+++ core/base.py
@@ -1,1 +1,11 @@
+from abc import ABC, abstractmethod
 
+class TextGenerator(ABC):
+    @abstractmethod
+    def generate(self, topic: str) -> str:
+        pass
+
+class ImageGenerator(ABC):
+    @abstractmethod
+    def generate(self, prompt: str, **kwargs) -> str:
+        pass
