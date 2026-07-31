9f6bbb11f55bc0622df6ec03379970778174c256
marketplaysru-stack <marketplaysru@gmail.com>
2026-07-31 13:42:24 +0300

    Delete models/post.py

--- models/post.py
+++
@@ -1,9 +0,0 @@
-"""models/post.py"""
-from dataclasses import dataclass
-from typing import Optional
-
-@dataclass
-class Post:
-    text: str
-    image_url: Optional[str] = None
-    image_bytes: Optional[bytes] = None
