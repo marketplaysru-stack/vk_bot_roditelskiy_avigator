7dbf3d6e87868db74f8d88aef4f393b7b0fde05b
marketplaysru-stack <marketplaysru@gmail.com>
2026-08-01 10:30:30 +0300

    Delete generators/image/picsum.py

--- generators/image/picsum.py
+++
@@ -1,8 +0,0 @@
-import requests, os
-from core.base import ImageGenerator
-class PicsumGenerator(ImageGenerator):
-    def generate(self, prompt="", negative_prompt="", **kwargs):
-        url = f"https://picsum.photos/1024/1024?random={hash(prompt)}"
-        resp = requests.get(url, timeout=30)
-        resp.raise_for_status()
-        return resp.content
